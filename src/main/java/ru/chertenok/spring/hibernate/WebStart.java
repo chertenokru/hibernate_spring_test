e805b90ea2aeb7c58f016d99b561ee49a310e36f
chertenokru <admin@chertenok.ru>
2018-10-31 01:00:21 +0300

    welcome to spring boot problem :)

--- src/main/java/ru/chertenok/spring/hibernate/WebStart.java
+++ src/main/java/ru/chertenok/spring/hibernate/WebStart.java
@@ -1,37 +1,12 @@
 package ru.chertenok.spring.hibernate;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
-import javax.servlet.DispatcherType;
-import java.net.URL;
-import java.security.ProtectionDomain;
-import java.util.EnumSet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class WebStart {
-    //    public static Logger logger =  Logger.getLogger(Main.class);
-    public static final int PORT = 8080;
-
     public static void main(String[] args) {
-
-        Server server = new Server(PORT);
-
-        ProtectionDomain domain = WebStart.class.getProtectionDomain();
-        URL location = domain.getCodeSource().getLocation();
-
-        WebAppContext webapp = new WebAppContext();
-        webapp.setContextPath("/");
-        webapp.setWar(location.toExternalForm());
-        webapp.addFilter(new FilterHolder(new DelegatingFilterProxy("springSecurityFilterChain")), "/*", EnumSet.allOf(DispatcherType.class));
-        server.setHandler(webapp);
-        try {
-            server.start();
-            server.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        SpringApplication.run(WebStart.class, args);
     }
 }
